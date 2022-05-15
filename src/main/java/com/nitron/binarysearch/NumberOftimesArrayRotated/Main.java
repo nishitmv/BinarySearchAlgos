3aeaf11c1020a4130d9ab15b63c88c46ce502784
nishit <nishitmv@gmail.com>
2022-05-15 13:19:44 +0530

    Num of times array rotated

---
+++ src/main/java/com/nitron/binarysearch/NumberOftimesArrayRotated/Main.java
@@ -0,0 +1,11 @@
+package com.nitron.binarysearch.NumberOftimesArrayRotated;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {7, 9, 11, 12, 15};
+
+        System.out.println(new NumTimesSortedArrRotated().calc(arr));
+
+    }
+}
---
+++ src/main/java/com/nitron/binarysearch/NumberOftimesArrayRotated/NumTimesSortedArrRotated.java
@@ -0,0 +1,44 @@
+package com.nitron.binarysearch.NumberOftimesArrayRotated;
+
+// Find index of min element that will be num of times array rotated .
+public class NumTimesSortedArrRotated {
+
+
+    public int calc(int[] arr) {
+       return getIndexOfMinElement(arr, 0, arr.length-1, arr.length);
+    }
+
+    private int getIndexOfMinElement(int[] arr, int start, int end, int n)
+    {
+        if(arr[end]>arr[start])
+            return 0;
+
+        if(n==1)
+            return 0;
+
+        if(arr[end]<arr[start]&& arr[end]<arr[end-1])
+            return end;
+
+        if(arr[start]>arr[start+1])
+            return 1;
+
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+
+            if(arr[mid]<arr[(mid+1)%n] && arr[mid]<arr[(mid+n-1)%n])
+                return mid;
+
+            if(arr[mid]<=arr[end])
+            {
+                end = mid-1;
+            }
+            else if(arr[mid]>=arr[start])
+            {
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+
+}
