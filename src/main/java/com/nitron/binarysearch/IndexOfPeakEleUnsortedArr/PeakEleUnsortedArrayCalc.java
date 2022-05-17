7287c155ba436cd2c873083e07b8b99e434ac737
nishit <nishitmv@gmail.com>
2022-05-17 23:34:06 +0530

    Find Element in bitonic array

--- src/main/java/com/nitron/binarysearch/IndexOfPeakEleUnsortedArr/PeakEleUnsortedArrayCalc.java
+++ src/main/java/com/nitron/binarysearch/IndexOfPeakEleUnsortedArr/PeakEleUnsortedArrayCalc.java
@@ -22,12 +22,12 @@ public class PeakEleUnsortedArrayCalc {
                     return mid;
                 else return mid-1;
 
-             if(mid == 0)
+             else if(mid == 0)
                  if(nums[0]>nums[1])
                      return 0;
                  else return 1;
 
-            if(mid>0 && mid <  size-1) {
+            else if(mid>0 && mid < size-1) {
 
                 if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1])
                     return mid;
